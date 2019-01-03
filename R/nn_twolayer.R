a013437746950c838c1c7a445a3331359f0fde25
pchest <pjc468@nyu.edu>
2019-01-03 15:40:20 -0500

    Adding folder

--- R/nn_twolayer.R
+++ R/nn_twolayer.R
@@ -12,13 +12,15 @@
 #' @export
 #' nn_twolayer()
 
-nn_twolayer <- function(Words = 3000, Text = man_dat2$text, Codes = man_dat2$cmp_code2, Epochs = 2, Weighting = "count", Seed = 17, Batch = 32){
+nn_twolayer <- function(Words = 3000, Text = man_dat2$text[1:2000], Codes = man_dat2$cmp_code2[1:2000], Epochs = 2, Weighting = "count", Seed = 17,Units = 512, Batch = 32, CM = TRUE, Dropout = .5, Valsplit = .1){
   set.seed(Seed)
-  if(length(Text != length(Codes))){
+  require(caret)
+  require(keras)
+  require(dplyr)
+  if(length(Text) != length(Codes)){
     cat("The length of the text and codes variables aren't the same.")
     break
   }
-#  man_dat_sub <- sample_n(Data,size = Sample,replace = F)
   train_index <- sample(1:length(Text),size = length(Text)*.5,replace = F)
   Codes2 <- as.numeric(as.factor(Codes))
   con_train_x <- Text[train_index]
@@ -26,19 +28,27 @@ nn_twolayer <- function(Words = 3000, Text = man_dat2$text, Codes = man_dat2$cmp
   con_test_x <- Text[-train_index]
   con_test_y <- Codes2[-train_index]
   classes <- length(unique(Codes))+1
-  tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r",lower = T,num_words = Words) %>% 
-    fit_text_tokenizer(con_train_x)
-  txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = "count")
-  txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = "count")
-  length(unique(con_train_y)) ; length(unique(con_test_y))
+  if(Words != 0){
+    tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r",lower = T,num_words = Words) %>% 
+      fit_text_tokenizer(con_train_x)
+    txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = "count")
+    txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = "count")
+  } else {
+    tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r",lower = T) %>% 
+      fit_text_tokenizer(con_train_x)
+    txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = "count")
+    txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = "count")
+    Words = dim(txt_train)[2]
+    cat("There were ",Words," features used to train this model.")
+  }
+    
   train_y <- to_categorical(as.numeric(con_train_y),num_classes = classes)
   test_y <- to_categorical(as.numeric(con_test_y), num_classes = classes)
-  
   model <- keras_model_sequential() 
   model %>%
-    layer_dense(units = 512, input_shape = Words) %>% 
+    layer_dense(units = Units, input_shape = Words) %>% 
     layer_activation(activation = 'relu') %>% 
-    layer_dropout(rate = 0.5) %>% 
+    layer_dropout(rate = Dropout) %>% 
     layer_dense(units = classes) %>% 
     layer_activation(activation = 'softmax')
   
@@ -53,13 +63,20 @@ nn_twolayer <- function(Words = 3000, Text = man_dat2$text, Codes = man_dat2$cmp
     batch_size = Batch,
     epochs = Epochs,
     verbose = 1,
-    validation_split = 0.1
+    validation_split = Valsplit
   )
   score <- model %>% evaluate(
     txt_test, test_y,
     batch_size = Batch,
     verbose = 1
   )
-  return(score)
+  if(CM == FALSE){
+    return(score)
+  } else {
+    pred_class <- predict_classes(model, txt_test, batch_size = Batch)
+    score$ConMat <- caret::confusionMatrix(factor(pred_class,labels = levels(as.factor(Codes)),levels = 1:length(unique(Codes))),
+                                           factor(con_test_y,labels = levels(as.factor(Codes)),levels = 1:length(unique(Codes))))
+    return(score)
+  }
 }
-
+?layer_dense
