a12ea7784e39ff79523966f1155dfdcda7ad52d7
Patrick Chester <pjc468@nyu.edu>
2019-01-09 17:10:31 -0500

    Made code formatting consistent with quanteda standards and added params
    I made the formatting of my code more consistent with the formatting standards that have been implemented in the quanteda corpus. Additionally, I have taken what were fixed hyperparameters and converted them to variables that can be optimized in both the twolayer and lstm models.

--- R/nn_embedded_lstm.R
+++ R/nn_embedded_lstm.R
@@ -2,43 +2,53 @@
 #' 
 #' This function is a wrapper for a long term short term neural network written using the Keras Package.
 #'  
-#' @param Words The number of top words included in document feature matrixes used as training and testing data.
 #' @param Text The text that will be used as training and test data.
 #' @param Codes The codes that will be used as outcomes to be predicted by the NN model.
-#' @param Epochs The number of epochs used in the NN model.
+#' @param Words The number of top words included in document feature matrixes used as training and testing data.
 #' @param Seed The seed used in the model. Defaults to 17
+#' @param Train_prop The proportion of the data used to train the model. The remainder is used as test data.
+#' @param Epochs The number of epochs used in the NN model.
 #' @param Batch The number of batches estimated in the NN.
 #' @param MaxSentencelen All sentences will be truncated to this length to be input into the LSTM model
 #' @param WordEmbedDim The number of word embedding dimensions to be produced by the LSTM model
 #' @param ValSplit The validation split of the data used in the training of the LSTM model
+#' @param Units_lstm The number of network nodes used in the LSTM layer
+#' @param Dropout A floating variable bound between 0 and 1. It determines the rate at which units are dropped for the linear tranformation of the inputs.
+#' @param Recurrent_dropout A floating variable bound between 0 and 1. It determines the fraction of the units to drop for the linear transformation of the recurrent neural network layer.
 #' @param CM A logical variable that indicates whether a confusion matrix will be output from the function
 #' @param Model A logical variable that indicates whether the trained model should be included in the output of this function
 #' @keywords neural networks, LSTM
 #' @export
-nn_embedded_lstm <- function(Words = 3000, Text = man_dat2$text,Codes = man_dat2$cmp_code2, Epochs = 10, 
-                             Seed = as.numeric(Sys.time()), Batch = 32, MaxSentencelen = 60, WordEmbedDim = 50,ValSplit = .1, CM = TRUE, Model = FALSE){
+
+nn_embedded_lstm <- function(Text, Codes, 
+                             Words = 3000, Seed = 17, Train_prop = 0.5, 
+                             Epochs = 10, Batch = 32, MaxSentencelen = 60, WordEmbedDim = 50, ValSplit = 0.1, 
+                             Units_lstm = 64, Dropout = 0.2, Recurrent_dropout = 0.2, 
+                             CM = TRUE, Model = FALSE) {
   set.seed(Seed)
-  train_index <- sample(1:length(Text),size = length(Text)*.5,replace = F)
+  train_index <- sample(1:length(Text), size = length(Text) * Train_prop, replace = FALSE)
   Codes2 <- as.numeric(as.factor(Codes))
+  
   con_train_x <- Text[train_index]
   con_train_y <- Codes2[train_index]
   con_test_x <- Text[-train_index]
   con_test_y <- Codes2[-train_index]
-  classes <- length(unique(man_dat_sub$cmp_code2))+1
-  tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r",lower = T,num_words = Words) %>% 
+  classes <- length(unique(Codes2)) + 1
+  
+  tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r", 
+                        lower = TRUE, num_words = Words) %>% 
     fit_text_tokenizer(con_train_x)
-  txt_train <- texts_to_sequences(tok,texts = con_train_x) %>% pad_sequences(maxlen = MaxSentencelen)
-  txt_test <- texts_to_sequences(tok,texts = con_test_x) %>% pad_sequences(maxlen = MaxSentencelen)
   
-  
-  train_y <- to_categorical(as.numeric(con_train_y),num_classes = classes)
+  txt_train <- texts_to_sequences(tok, texts = con_train_x) %>% pad_sequences(maxlen = MaxSentencelen)
+  txt_test <- texts_to_sequences(tok, texts = con_test_x) %>% pad_sequences(maxlen = MaxSentencelen)
+  train_y <- to_categorical(as.numeric(con_train_y), num_classes = classes)
   test_y <- to_categorical(as.numeric(con_test_y), num_classes = classes)
   
   model <- keras_model_sequential() 
   model %>%
-      layer_embedding(input_dim = Words,output_dim = WordEmbedDim, input_length = MaxSentencelen) %>%
-      layer_lstm(units = 64, dropout = 0.2, recurrent_dropout = 0.2) %>% 
-      layer_dense(units = 58, activation = 'sigmoid')
+      layer_embedding(input_dim = Words, output_dim = WordEmbedDim, input_length = MaxSentencelen) %>%
+      layer_lstm(units = Units_lstm, dropout = Dropout, recurrent_dropout = Recurrent_dropout) %>% 
+      layer_dense(units = classes, activation = 'sigmoid')
 
   model %>% compile(
     loss = 'categorical_crossentropy',
@@ -60,8 +70,12 @@ nn_embedded_lstm <- function(Words = 3000, Text = man_dat2$text,Codes = man_dat2
   )
   if(CM == TRUE){
       pred_class <- predict_classes(model, txt_test, batch_size = Batch)
-      score$ConMat <- caret::confusionMatrix(factor(pred_class,labels = levels(as.factor(Codes)),levels = 1:length(unique(Codes))),
-                                             factor(con_test_y,labels = levels(as.factor(Codes)),levels = 1:length(unique(Codes))))
+      score$ConMat <- caret::confusionMatrix(factor(pred_class,
+                                                    labels = levels(as.factor(Codes)), 
+                                                    levels = 1:length(unique(Codes))),
+                                             factor(con_test_y,
+                                                    labels = levels(as.factor(Codes)), 
+                                                    levels = 1:length(unique(Codes))))
   } 
   if(Model == TRUE){
       score$Model <- model
--- R/nn_twolayer.R
+++ R/nn_twolayer.R
@@ -1,47 +1,58 @@
 #' Neural Network with Two Layers
 #' 
 #' This function is a wrapper for a two layered neural network written using the Keras Package. It takes a 
-#' @param Words The number of top words included in document feature matrixes used as training and testing data.
 #' @param Text The text that will be used as training and test data.
 #' @param Codes The codes that will be used as outcomes to be predicted by the NN model.
-#' @param Epochs The number of epochs used in the NN model.
-#' @param Weighting The type of feature weighting used in the document feature matrix. I.e., count and tfidf.
+#' @param Words The number of top words included in document feature matrixes used as training and testing data.
 #' @param Seed The seed used in the model. Defaults to 17
-#' @param Batch The number of batches estimated in the NN.
+#' @param Weighting The type of feature weighting used in the document feature matrix. I.e., count and tfidf.
+#' @param Train_prop The proportion of the data used to train the model. The remainder is used as test data.
+#' @param Epochs The number of epochs used in the NN model.
+#' @param Units The number of network nodes used in the first layer of the sequential model
+#' @param Batch The number of batches estimated
+#' @param Dropout A floating variable bound between 0 and 1. It determines the rate at which units are dropped for the linear tranformation of the inputs.
+#' @param ValSplit The validation split of the data used in the training of the LSTM model
+#' @param CM A logical variable that indicates whether a confusion matrix will be output from the function
+#' @param Model A logical variable that indicates whether the trained model should be included in the output of this function
 #' @keywords neural networks
 #' @export
 
-nn_twolayer <- function(Words = 3000, Text = man_dat2$text[1:2000], Codes = man_dat2$cmp_code2[1:2000], Epochs = 2, Weighting = "count", Seed = 17,Units = 512, Batch = 32, CM = TRUE, Dropout = .5, Valsplit = .1){
+nn_twolayer <- function(Text, Codes, 
+                        Words = 3000, Seed = 17, Weighting = "count", Train_prop = .5, 
+                        Epochs = 2, Units = 512, Batch = 32, Dropout = .5, Valsplit = .1,  
+                        CM = TRUE, Model = FALSE){
   set.seed(Seed)
   require(caret)
   require(keras)
   require(dplyr)
-  if(length(Text) != length(Codes)){
+  if(length(Text) != length(Codes)) {
     cat("The length of the text and codes variables aren't the same.")
     break
   }
-  train_index <- sample(1:length(Text),size = length(Text)*.5,replace = F)
+  train_index <- sample(1:length(Text),size = length(Text) * Train_prop, replace = F)
   Codes2 <- as.numeric(as.factor(Codes))
+  
   con_train_x <- Text[train_index]
   con_train_y <- Codes2[train_index]
   con_test_x <- Text[-train_index]
   con_test_y <- Codes2[-train_index]
-  classes <- length(unique(Codes))+1
+  classes <- length(unique(Codes2)) + 1
+  
   if(Words != 0){
     tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r",lower = T,num_words = Words) %>% 
       fit_text_tokenizer(con_train_x)
-    txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = "count")
-    txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = "count")
+    txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = Weighting)
+    txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = Weighting)
   } else {
     tok <- text_tokenizer(filters = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r",lower = T) %>% 
       fit_text_tokenizer(con_train_x)
-    txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = "count")
-    txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = "count")
+    txt_train <- texts_to_matrix(tok,texts = con_train_x, mode = Weighting)
+    txt_test <- texts_to_matrix(tok,texts = con_test_x, mode = Weighting)
     Words = dim(txt_train)[2]
     cat("There were ",Words," features used to train this model.")
   }
     
-  train_y <- to_categorical(as.numeric(con_train_y),num_classes = classes)
+  train_y <- to_categorical(as.numeric(con_train_y), num_classes = classes)
   test_y <- to_categorical(as.numeric(con_test_y), num_classes = classes)
   model <- keras_model_sequential() 
   model %>%
@@ -69,12 +80,17 @@ nn_twolayer <- function(Words = 3000, Text = man_dat2$text[1:2000], Codes = man_
     batch_size = Batch,
     verbose = 1
   )
-  if(CM == FALSE){
-    return(score)
-  } else {
+  if(CM == TRUE) {
     pred_class <- predict_classes(model, txt_test, batch_size = Batch)
-    score$ConMat <- caret::confusionMatrix(factor(pred_class,labels = levels(as.factor(Codes)),levels = 1:length(unique(Codes))),
-                                           factor(con_test_y,labels = levels(as.factor(Codes)),levels = 1:length(unique(Codes))))
-    return(score)
+    score$ConMat <- caret::confusionMatrix(factor(pred_class,
+                                                  labels = levels(as.factor(Codes)), 
+                                                  levels = 1:length(unique(Codes))),
+                                           factor(con_test_y,
+                                                  labels = levels(as.factor(Codes)), 
+                                                  levels = 1:length(unique(Codes))))
+  } 
+  if(Model == TRUE) {
+    score$Model <- model
   }
+  return(score)
 }
